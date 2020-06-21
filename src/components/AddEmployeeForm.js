ab85608ac19feb152923f285a0950a132a58d8a4
Vado <vadvoker@gmail.com>
2020-06-21 09:47:45 +0300

    refactor input component

--- src/components/AddEmployeeForm.js
+++ src/components/AddEmployeeForm.js
@@ -4,7 +4,7 @@ function getLengthValidaion(valuesMap, values) {
   let errors = [];
 
   Object.keys(valuesMap).forEach((key) => {
-    if (values[key].length < valuesMap[key]) {
+    if (values[key].trim().length < valuesMap[key]) {
       errors.push(`${key} value should be longer than ${valuesMap[key]} characters`);
     }
   });
@@ -25,7 +25,7 @@ const createSubmitFunc = (handleSubmit) => (e) => {
     creator: creator.value
   };
 
-  const errors = getLengthValidaion({ creator: 3, name: 3, email: 3 }, values);
+  const errors = getLengthValidaion({ creator: 2, name: 3, email: 3 }, values);
 
   if (errors.length) {
     alert(errors[0]);
@@ -38,17 +38,17 @@ const createSubmitFunc = (handleSubmit) => (e) => {
 export function AddEmployeeForm({ handleSubmit }) {
   return (
     <form onSubmit={createSubmitFunc(handleSubmit)} className="p-8 bg-gray-300 rounded">
-      <InputComponent name="Creator" required></InputComponent>
+      <InputComponent name="creator" required></InputComponent>
 
-      <InputComponent name="Name" required></InputComponent>
+      <InputComponent name="name" required></InputComponent>
 
-      <InputComponent name="City"></InputComponent>
+      <InputComponent name="city"></InputComponent>
 
-      <InputComponent name="Address"></InputComponent>
+      <InputComponent name="address"></InputComponent>
 
-      <InputComponent name="Phone"></InputComponent>
+      <InputComponent name="phone"></InputComponent>
 
-      <InputComponent name="Email" required></InputComponent>
+      <InputComponent name="email" required></InputComponent>
 
       <button className="bg-gray-700 px-5 py-1 text-white" type="submit">
         SUBMIT
@@ -59,12 +59,13 @@ export function AddEmployeeForm({ handleSubmit }) {
 
 function InputComponent({ name, required }) {
   const requiredText = required ? <span className="text-red-700">*</span> : null;
+  const labelName = name[0].toUpperCase() + name.slice(1);
   return (
     <label className="flex mb-5">
       <p className={`w-20 mr-3`}>
-        {name} {requiredText}:
+        {labelName} {requiredText}:
       </p>
-      <input className="px-3 py-1 rounded" name={name.toLowerCase()} placeholder={`${name}`}></input>
+      <input className="px-3 py-1 rounded" name={name} placeholder={` Employee ${labelName}`}></input>
     </label>
   );
 }

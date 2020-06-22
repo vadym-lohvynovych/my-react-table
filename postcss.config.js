8301351e45782d7354d5df843126a23caadc8ed7
Vado <vadvoker@gmail.com>
2020-06-22 09:04:24 +0300

    remove unused properties from postcss.config

--- postcss.config.js
+++ postcss.config.js
@@ -3,11 +3,9 @@ const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
 
 const purgecssConfig = {
-  content: ['./src/index.html', './src/**/*.svelte', './src/**/*.js'],
-  whitelist: ['flatpickr'],
-  whitelistPatterns: [/svelte-/, /flatpickr/, /span/],
+  content: ['./src/index.html', './src/**/*.js'],
   whitelistPatternsChildren: [/flatpickr/],
-  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
 };
 
 const plugins = process.env.NODE_ENV === 'production' ? [tailwindcss('./tailwind.config.js'), autoprefixer(), purgecss(purgecssConfig)] : [tailwindcss('./tailwind.config.js')];

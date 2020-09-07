2da3655d2c2a5d3f2fd885fb47ed6fb2d932b2ca
unknown <vadvoker@gmail.com>
2020-09-07 17:43:34 +0300

    add tailwind to dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,7 @@ const autoprefixer = require('autoprefixer');
 
 const purgecssConfig = {
   content: ['./src/index.html', './src/**/*.js'],
-  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
 };
 
 const plugins = process.env.NODE_ENV === 'production' ? [tailwindcss('./tailwind.config.js'), autoprefixer(), purgecss(purgecssConfig)] : [tailwindcss('./tailwind.config.js')];
